e991454304db737a8e6f0ccfafdebf99e2dd3730
gyu <gyu@gyuui-MacBookPro.local>
2024-06-26 01:41:27 +0900

    add permit

--- src/interfaces/IWNAD.sol
+++ src/interfaces/IWNAD.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.20;
 
 interface IWNAD {
     function deposit() external payable;
-    function transfer(address to, uint256 value) external returns (bool);
+    // function transfer(address to, uint256 value) external returns (bool);
     function withdraw(uint256) external;
+    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
+        external;
 }
