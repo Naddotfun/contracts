c409d1758af54df0f7236be8af5a6d5e377a749b
gyu <gyu@gyuui-MacBookPro.local>
2025-01-10 16:32:08 +0900

    Refactor : UniswapV2Router

--- src/uniswap/UniswapV2Router.sol
+++ src/uniswap/UniswapV2Router.sol
@@ -6,8 +6,9 @@ import "./interfaces/IUniswapV2Factory.sol";
 import "./interfaces/IUniswapV2Pair.sol";
 import "./libraries/UniswapV2Library.sol";
 import "../interfaces/IWNative.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+
+import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {TransferHelper} from "../utils/TransferHelper.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
